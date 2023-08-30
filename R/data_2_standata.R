00ee1f4cfb177530632ae8c87d1eb9a3d97f0b56
a-wis <a.wisniowski@manchester.ac.uk>
2023-08-30 12:46:17 +0100

    update data function

--- R/data_2_standata.R
+++ R/data_2_standata.R
@@ -14,53 +14,53 @@ data_2_standata = function(df, sending = c("SE","FI","IT","PL"),
                            years = 2010:2019,
                            ref.country="SE"){
   df <- df %>%
-    filter(Sending_iso2 %in% sending,
+    dplyr::filter(Sending_iso2 %in% sending,
            Receiving_iso2 %in% receiving,
            Year %in% years) %>%
-    mutate(corr_f=Corridor %>% as.factor() %>% as.numeric(),
+    dplyr::mutate(corr_f=Corridor %>% as.factor() %>% as.numeric(),
            orig=Sending %>% as.factor() %>% as.numeric(),
            dest=Receiving %>% as.factor() %>% as.numeric(),
            benchmark=ifelse(Receiving_iso2==ref.country,1,0))
 
   #data undercounting auxiliary objects
   XL_em=df %>%
-    filter(!is.na(Emig)) %>%
-    select(Emig,UE) %>%
+    dplyr::filter(!is.na(Emig)) %>%
+    dplyr::select(Emig,UE) %>%
     model.matrix(Emig~UE-1,data=.)
 
   XL_im=df %>%
-    filter(!is.na(Immi)) %>%
-    select(Immi,UI,benchmark) %>%
+    dplyr::filter(!is.na(Immi)) %>%
+    dplyr::select(Immi,UI,benchmark) %>%
     model.matrix(Immi~benchmark+UI-1,data=.)
   XL_im[,2]=XL_im[,2]-XL_im[,1]
   XL_im=XL_im[,2:4]
 
   out=list(N_mis=df %>%
-             filter(is.na(Immi)& is.na(Emig)) %>%
+             dplyr::filter(is.na(Immi)& is.na(Emig)) %>%
              pull(Emig) %>%
              length(), # N missing E & I
            N_obs=df %>%
-             filter(Year<2020 & (!is.na(Immi) | !is.na(Emig))) %>%
+             dplyr::filter(Year<2020 & (!is.na(Immi) | !is.na(Emig))) %>%
              pull(Immi) %>% length(), # N obs E & I
            N_all= dim(df)[1], # N all
            N_obs1=df %>%
-             filter(Year<2020, !is.na(Immi)) %>%
+             dplyr::filter(Year<2020, !is.na(Immi)) %>%
              pull(Immi) %>% length(), # N obs I
            N_mis1=df %>%
              pull(Immi) %>% is.na() %>%
              sum(), # N missing I
            N_obs2=df %>%
-             filter(Year<2020, !is.na(Emig)) %>%
+             dplyr::filter(Year<2020, !is.na(Emig)) %>%
              pull(Emig) %>% length(), # N obs E
            N_mis2=df %>%
              pull(Emig) %>% is.na() %>%
              sum(), # N missing E
            N_preds=3, # No. predictors
            z1=df %>%
-             filter(Year<2020, !is.na(Immi)) %>%
+             dplyr::filter(Year<2020, !is.na(Immi)) %>%
              pull(Immi), # data I
            z2=df %>%
-             filter(Year<2020, !is.na(Emig)) %>%
+             dplyr::filter(Year<2020, !is.na(Emig)) %>%
              pull(Emig), # data E
            z_ind1 = df %>%
              select(Immi) %>%
@@ -80,11 +80,11 @@ data_2_standata = function(df, sending = c("SE","FI","IT","PL"),
            N_corr=length(unique(df$Corridor)), # N corridors
            acc_e=df %>%
              mutate(acc_E=as.numeric(fct_recode(AE,"Low"="Medium"))) %>%
-             filter(!is.na(Emig)) %>%
+             dplyr::filter(!is.na(Emig)) %>%
              pull(acc_E), # accuracy index E
            acc_i=df %>%
              mutate(acc_I=as.numeric(fct_recode(AI,"Low"="Medium"))) %>%
-             filter(!is.na(Immi)) %>%
+             dplyr::filter(!is.na(Immi)) %>%
              pull(acc_I), # accuracy index I
            XL_e=XL_em,
            XL_i=XL_im,
