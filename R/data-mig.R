6b456701c04263855989ed544a1f56197b816712
a-wis <a.wisniowski@manchester.ac.uk>
2023-08-31 23:37:48 +0100

    update documentation

--- R/data-mig.R
+++ R/data-mig.R
@@ -2,18 +2,19 @@
 #'
 #' These data sets have been downloaded from:
 #' the publicly available Eurostat data base on population:
-#' [https://ec.europa.eu/eurostat/web/main/data/database],
+#' \href{https://ec.europa.eu/eurostat/web/main/data/database}{https://ec.europa.eu/eurostat/web/main/data/database},
 #' data bases 'migr' and 'demo'.
 #' - These are migration statistics for selected EU countries,
 #' and population sizes. The data relate to 2010-2019 period.
 #' - Shiny app made by Maciej Jan Dańko:
-#' [https://maciej-jan-danko.shinyapps.io/HMigD_Shiny_App_I/],
+#' \href{https://maciej-jan-danko.shinyapps.io/HMigD_Shiny_App_I/}{https://maciej-jan-danko.shinyapps.io/HMigD_Shiny_App_I/},
 #' These data contain the assumed accuracy of the data sources.
 #' - Shiny app made by Maciej Jan Dańko:
-#' [https://maciej-jan-danko.shinyapps.io/undercountmigscores/]
+#' \href{https://maciej-jan-danko.shinyapps.io/undercountmigscores/}{https://maciej-jan-danko.shinyapps.io/undercountmigscores/}
 #' These data contain undercounting scores for emigration
 #' and immigration based on 30% metadata and 70% model scores.
-#'
+#' - Data on distance between countries from the CEPII database
+#' \href{http://cepii.fr/CEPII/en/bdd_modele/bdd_modele_item.asp?id=6}{cepii.fr/CEPII/}.
 #'
 #' `mig_data` are a data frame/tibble.
 #'
--- R/migmod_m.R
+++ R/migmod_m.R
@@ -1,13 +1,28 @@
 #' Function to estimate migration model
 #'
+#' @description The model estimates 'true', unobserved bilateral migration flows \eqn{y_{ijt}} from sending country \eqn{i} to receiving country \eqn{j} in year \eqn{t}. It relies on data reported by receiving countries (immigration, \eqn{z_{ijt}^R}) and sending countries (emigration, \eqn{z_{ijt}^S}). It is based on methodology developed by Raymer \emph{et al}. (2013). The data are Poisson-distributed with mean \eqn{\mu_{ijt}^k}:
+#'
+#' @description \eqn{z_{ijt}^k\sim\mathrm{Poisson}\left(\mu_{ijt}^k\right)}{z_{ijt}^k\sim\mathrm{Poisson}\left(\mu_{ijt}^k\right)}.
+#'
+#' @description The true migration flows then result from correcting the expected values of the data for biases represented by parameters \eqn{\lambda} and different accuracies of the sending and receiving countries' data \eqn{\sigma}:
+#'
+#' @description \eqn{\log\mu_{ijt}^k  \sim \mathrm{normal}\left(\log y_{ijt}+\lambda_{f(i,j)}, \sigma_{g(i,j)}^k\right)}{\log\mu_{ijt}^k  \sim \mathrm{normal}\left(\log y_{ijt}+\lambda_{f(i,j)}, \sigma_{g(i,j)}^k\right)}.
+#'
+#' @description The true flows are then modelled by using a mixed effects model that contains an autoregressive part with parameter \eqn{\phi} and a gravity-type model with populations of sending (\eqn{X_{it}}) and receiving (\eqn{X_{jt}}) countries as well as distance \eqn{D_{ij}} between them:
+#'
+#' @description \eqn{\log y_{ijt}^k  \sim \mathrm{normal}\left(\phi_{ij}\log y_{ijt-1} + \beta_1 X_{it} + \beta_2 X_{jt} + \beta_3 D_{ij}, \sigma_y\right)}
+#'
+#' @description Note, that this specification requires that there are no fully unobserved flows in the model.
 #' @export
 #' @param df A data frame with migration data for sending countries, receiving countries, data quality measures and covariates
 #' @param sending A vector of ISO2 codes with sending countries to be used in the model (recommended to keep SE and FI)
 #' @param receiving A vector of ISO2 codes with receiving countries to be used in the model (recommended to keep SE and FI)
 #' @param years A vector of years to be used in the model (2010 to 2019).
 #' @param ref.country A character with an ISO2 code receiving country which immigration data are is used as a reference. This assumes that the data reported by this country are not biased. This assumption ensures identification of model parameters. It is strongly recommended to use Sweden ("SE") as a reference.
-#' @param ... Arguments passed to function [rstan::sampling()] from package \pkg{\link{rstan}} (e.g. iter, chains).
+#' @param ... Arguments passed to function \code{\link[rstan]{sampling}} from package \pkg{\link{rstan}} (e.g. iter, chains).
 #' @return An object of class `stanfit` returned by `rstan::sampling`
+#'
+#' @references Raymer J., Wiśniowski A., Forster J. J., Smith P. W. & Bijak J. (2013). Integrated modeling of European migration. \emph{Journal of the American Statistical Association}, 108(503), 801-819. \href{https://doi.org/10.1080/01621459.2013.789435}{https://doi.org/10.1080/01621459.2013.789435}.
 #'
 #' @seealso \code{\link[rstan]{stan}}
 migmod_m <- function(df,
